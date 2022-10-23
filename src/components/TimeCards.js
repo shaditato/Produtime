986548bde5250a9df32bfcf3613689c87f404890
shaditato <79299258+shaditato@users.noreply.github.com>
2022-10-22 23:10:44 -0400

    Add divider, update README

--- src/components/TimeCards.js
+++ src/components/TimeCards.js
@@ -5,6 +5,7 @@ import {
   Card,
   CardActions,
   CardContent,
+  Divider,
   Typography,
 } from "@mui/material";
 import { GlobalContext } from "../context/GlobalState";
@@ -24,55 +25,58 @@ export function TimeCards() {
   }
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flex: 1,
-        padding: 1,
-        overflow: "auto",
-        scrollbarWidth: "thin",
-        "&::-webkit-scrollbar": {
-          height: "10px",
-        },
-        "&::-webkit-scrollbar-thumb": {
-          borderRadius: "10px",
-          backgroundColor: "#D3D3D3",
-        },
-      }}
-    >
-      <Box sx={{ display: "flex", minHeight: "min-content" }}>
-        {activeTimers.map((timer) => {
-          const project = projects[timer.projectId] || {};
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flex: 1,
+          padding: 1,
+          overflow: "auto",
+          scrollbarWidth: "thin",
+          "&::-webkit-scrollbar": {
+            height: "10px",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            borderRadius: "10px",
+            backgroundColor: "#D3D3D3",
+          },
+        }}
+      >
+        <Box sx={{ display: "flex", minHeight: "min-content" }}>
+          {activeTimers.map((timer) => {
+            const project = projects[timer.projectId] || {};
 
-          return (
-            <Card
-              key={timer.createdAt}
-              sx={{
-                backgroundColor: project.colour,
-                margin: 1,
-                width: 275,
-              }}
-            >
-              <CardContent>
-                <ActiveTimer offsetTimestamp={timer.createdAt} />
-                <Typography variant="subtitle1" color="text.secondary">
-                  {project.name}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  color="inherit"
-                  data-created={timer.createdAt}
-                  onClick={endTimer}
-                  startIcon={<Stop />}
-                >
-                  Stop Timer
-                </Button>
-              </CardActions>
-            </Card>
-          );
-        })}
+            return (
+              <Card
+                key={timer.createdAt}
+                sx={{
+                  backgroundColor: project.colour,
+                  margin: 1,
+                  width: 275,
+                }}
+              >
+                <CardContent>
+                  <ActiveTimer offsetTimestamp={timer.createdAt} />
+                  <Typography variant="subtitle1" color="text.secondary">
+                    {project.name}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    color="inherit"
+                    data-created={timer.createdAt}
+                    onClick={endTimer}
+                    startIcon={<Stop />}
+                  >
+                    Stop Timer
+                  </Button>
+                </CardActions>
+              </Card>
+            );
+          })}
+        </Box>
       </Box>
-    </Box>
+      {activeTimers.length > 0 && <Divider variant="middle" />}
+    </>
   );
 }
