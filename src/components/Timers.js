bdfb11396d4bc557c071944e863efec1229c58d1
shaditato <79299258+shaditato@users.noreply.github.com>
2023-01-17 00:27:35 -0500

    Group timers by date

--- src/components/Timers.js
+++ src/components/Timers.js
@@ -1,5 +1,12 @@
 import { useContext, useEffect } from "react";
-import { Card, CardActionArea, Chip, Toolbar, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  Chip,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { GlobalContext } from "../context/GlobalState";
 import { msToHMS } from "../utils/format";
 
@@ -10,22 +17,55 @@ export function Timers() {
     getTimers({ uid: user.uid });
   }, []);
 
+  // Create an object to map timers to date strings
+  const datedTimers = timers.reduce((accumulator, timer) => {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const date = new Date(timer.createdAt).toLocaleDateString(
+      undefined,
+      options
+    );
+
+    if (accumulator[date] === undefined) {
+      return {
+        ...accumulator,
+        [date]: [timer],
+      };
+    }
+
+    return {
+      ...accumulator,
+      [date]: [timer, ...accumulator[date]],
+    };
+  }, {});
+
   return (
     <>
-      {timers.map((timer) => (
-        <Card elevation={0}>
-          <CardActionArea>
-            <Toolbar>
-              <Chip
-                label={msToHMS(timer.endedAt - timer.createdAt)}
-                sx={{ backgroundColor: projects[timer.projectId].colour }}
-              />
-              <Typography sx={{ marginX: 1 }}>
-                {projects[timer.projectId].name}
-              </Typography>
-            </Toolbar>
-          </CardActionArea>
-        </Card>
+      {Object.keys(datedTimers).map((date) => (
+        <Box key={date}>
+          <Typography sx={{ marginX: 3 }} variant="overline">
+            {date}
+          </Typography>
+          {datedTimers[date].map((timer) => (
+            <Card elevation={0} key={timer.createdAt}>
+              <CardActionArea>
+                <Toolbar>
+                  <Chip
+                    label={msToHMS(timer.endedAt - timer.createdAt)}
+                    sx={{ backgroundColor: projects[timer.projectId].colour }}
+                  />
+                  <Typography sx={{ marginX: 1 }}>
+                    {projects[timer.projectId].name}
+                  </Typography>
+                </Toolbar>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Box>
       ))}
     </>
   );
