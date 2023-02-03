4a28adf137eedafaa8a6728a16ea0a0f9e642884
shaditato <79299258+shaditato@users.noreply.github.com>
2023-02-03 18:53:39 -0500

    Update TimerMenu styles

--- src/components/TimerDialog/TimerMenu/index.js
+++ src/components/TimerDialog/TimerMenu/index.js
@@ -1,20 +1,36 @@
-import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { Delete, Label, Notes } from "@mui/icons-material";
 
 export function TimerMenu() {
   return (
     <List dense>
-      <ListItem disableGutters>
+      <ListItem disablePadding>
         <ListItemButton>
+          <ListItemIcon>
+            <Label />
+          </ListItemIcon>
           <ListItemText>Edit Tags</ListItemText>
         </ListItemButton>
       </ListItem>
-      <ListItem disableGutters>
+      <ListItem disablePadding>
         <ListItemButton>
+          <ListItemIcon>
+            <Notes />
+          </ListItemIcon>
           <ListItemText>Edit Description</ListItemText>
         </ListItemButton>
       </ListItem>
-      <ListItem disableGutters>
+      <ListItem disablePadding>
         <ListItemButton>
+          <ListItemIcon>
+            <Delete />
+          </ListItemIcon>
           <ListItemText>Delete Timer Record</ListItemText>
         </ListItemButton>
       </ListItem>
